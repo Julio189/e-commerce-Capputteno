f696eabd00dc39220d2539dfc7ec0eee4f8ef9b5
Julio189 <julio.c.jesus@hotmail.com>
2023-06-28 17:27:26 -0300

    feix: add cache time to request

--- capputteno/hooks/useProducts.ts
+++ capputteno/hooks/useProducts.ts
@@ -45,7 +45,8 @@ export const useProducts = () => {
     const query = mountQuery(type, priority)
     const { data } = useQuery({
         queryFn: () => fetcher(query),
-        queryKey:['products', type, priority]
+        queryKey:['products', type, priority],
+        staleTime: 1000 * 60 * 1
     })
 
     const products = data?.data?.data?.allProducts
